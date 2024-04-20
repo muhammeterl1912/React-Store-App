4b57a0268427dae61f0096b745219ba8ef919621
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-04-20 18:55:30 +0200

    structered folders and files and congifured to tailwind css.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+
+
+const Login = () => {
+  return (
+    <div className="h-[100vh] bg-yellow-400 flex justify-center items-center  ">
+        <form className="h-1/2 w-1/4 bg-white rounded-[1rem] p-6">
+            <h3>Erol Store</h3>
+            <h5>SIGN IN</h5>
+            <p>Enter your credentials to access your account</p>
+        <input type="email" />
+        </form>
+    </div>
+  )
+}
+
+export default Login
