e6f15cc5df1afd45353a96f0ab56acc88ffe549d
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-04-23 13:40:32 +0200

    get data from helpers finished about.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,14 @@
 import SearchInput from "../components/SearchInput";
+import { useParams,useNavigate } from "react-router-dom";
 
 
 const Products = () => {
+ const {id} =useParams()
+ console.log(id)
+ const navigate =useNavigate()
   return <div className="mt-5">
   <SearchInput/>
+  <button onClick={()=>{navigate(id)}}>tikla</button>
   </div>;
 };
 
